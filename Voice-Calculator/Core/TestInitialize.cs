92d4aebbe72368a5189bcb4bdbae05b1e26d36c5
NayabAkhtar12 <119120061+NayabAkhtar12@users.noreply.github.com>
2024-11-29 10:09:28 +0500

    Update TestInitialize.cs

--- Voice-Calculator/Core/TestInitialize.cs
+++ Voice-Calculator/Core/TestInitialize.cs
@@ -26,8 +26,7 @@ namespace ScientificCalculator.Core
             Cap.AddAdditionalCapability(MobileCapabilityType.Udid, "ce11171b9bd3d81105");
             Cap.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "9");
             Cap.AddAdditionalCapability("appium:automationName", AutomationName.AndroidUIAutomator2);
-            Cap.AddAdditionalCapability(AndroidMobileCapabilityType.AppActivity, "com.voice.calculator.qr.scanner.scientificcalculator.qrcode.barcode.reader.ScientificCal");
-            Cap.AddAdditionalCapability(MobileCapabilityType.App, "/data/app/com.voice.calculator.qr.scanner.scientificcalculator.qrcode.barcode.reader--MO3PCVgfH7n4Uv5R86seQ==/base.apk");
+
 
             //Navigate to App
             driver = new AndroidDriver<IWebElement>(new Uri("http://192.168.100.22:4723/"), Cap, TimeSpan.FromSeconds(180));
